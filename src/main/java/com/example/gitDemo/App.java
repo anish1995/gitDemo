5d0d90144d35fd3a069993d2ca1df0586ea19d37
anish kumar <ankushanish@gmail.com>
2019-07-10 10:37:10 +0530

    gitHub is working

--- src/main/java/com/example/gitDemo/App.java
+++ src/main/java/com/example/gitDemo/App.java
@@ -8,7 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-        System.out.println( "sdfjdshfksdfsfdg!" );
+        System.out.println("gitHub is working.");
     }
 }
